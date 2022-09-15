af5d14a42d98ae8d3b0c693e1f78c3263b0b4603
dtnfktu <vtlg@mail.ru>
2022-09-15 16:04:07 +0300

    All tasks complete

--- lesson04/program4.py
+++ lesson04/program4.py
@@ -5,16 +5,24 @@ def encrypt_text(txt : str, key : int) :
         text_out += chr(ord(txt[i]) + key)
     return text_out
 
-def decrypt_text(txt : str, key : int) :
-    '''Дешифрует текст txt со сдвигом на key позиций'''
-    text_out = ''
-    for i in range(0, len(txt)) :
-        text_out += chr(ord(txt[i]) - key)
-    return text_out
+def write_into_file(file_path : str, txt : str) :
+    '''Записывает текст в файл'''
+    with open(file_path, 'w') as file :
+        file.write(txt)
+
+def decrypt_text_from_file(filename : str) :
+    sk = input('Enter key : ')
+    while not sk.isdigit() :
+        sk = input('Enter key')
+    key = int(sk)
+
+    with open(filename,'r') as sourcefile :
+        txt = sourcefile.read()
+
+    return encrypt_text(txt, -key)
 
 
 ttext = input('Enter text : ')
-print(ttext)
 enc_text = encrypt_text(ttext, 5)
-print('Encrypted text : ' + enc_text)
-print('Decrypted text : ' + decrypt_text(enc_text, 5))
+write_into_file('caesar.txt', enc_text)
+print(decrypt_text_from_file('caesar.txt'))
