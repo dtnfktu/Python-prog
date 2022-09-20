eac9cec03093a897b016991b6f2a272967880d30
dtnfktu <vtlg@mail.ru>
2022-09-20 08:50:54 +0300

    Rask 3 lesson 5 complete

---
+++ lesson05/program3.py
@@ -0,0 +1,31 @@
+def lists_zip(list1, list2) :
+    '''Cоздаёт список кортежей, состоящих из номера и языка, написанного большими буквами'''
+    return list(zip(list1,  [x.upper() for x in list2]))
+
+def filter_list(tuple_list : list) :
+    '''Фильтрует список tuple_list следующим образом: если сумма очков слова имеет в делителях номер, 
+    с которым она в паре в кортеже, то кортеж остается, его номер заменяется на сумму очков. Если нет — удаляется.'''
+    #tuple_list = tuple_list
+
+    for i in range(len(tuple_list) - 1, 0, -1) :
+        
+        x = tuple_list[i]
+        points = sum(list(map(ord, list(x[1]))))    # вычисляем сумму очков текущего кортежа
+        
+        if points % x[0] != 0 :
+            tuple_list.remove(x)
+        else :
+            t = list(x)
+            t[0] = points
+            tuple_list[i] = tuple(t)
+
+    return tuple_list
+
+
+langs = ['python', 'C#', 'Java', 'C++', 'JavaScript', 'Go', 'Perl', 'Pascal']
+nums = [i for i in range(1, len(langs) + 1)]
+
+print('Source list')
+print(lists_zip(nums, langs))
+print('Resulting list')
+print(filter_list(lists_zip(nums, langs)))
