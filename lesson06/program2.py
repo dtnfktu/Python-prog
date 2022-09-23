6ff7130ce8ae1bfc9ac4a6c9b36634471df5577c
dtnfktu <vtlg@mail.ru>
2022-09-23 14:19:15 +0300

    Task 5 lesson 6 complete

--- lesson06/program2.py
+++ lesson06/program2.py
@@ -8,11 +8,5 @@ print(lst)
 # Нумеруем элементы и отсеиваем чётные индексы
 lt = list(filter(lambda x: x[0] % 2 != 0, list(enumerate(lst,start=0))))
 
-# Суммируем элементы и показываем её
+# Суммируем элементы и показываем
 print(f'Sum of odd elements = {sum(list(x[1] for x in lt))}')
-
-
-odd_list = [x for x in range(1, len(lst), 2)]
-print(odd_list)
-out = [lst[odd_list]]
-print(odd_list)
--- lesson06/program5.py
+++ lesson06/program5.py
@@ -3,8 +3,10 @@ import random
 n = 9
 
 lst = [random.randint(1, 10) for i in range(n)]
-print(lst)
+print(f'Исходный список :\n{lst}')
 
-mul = [lst[i] * lst[len(lst) - i - 1] for i in range(len(lst)//2)]
-
-print(mul)
+mul = [lst[i] * lst[len(lst) - 1 - i] for i in range(round(len(lst)/2 + 0.1))]
+# Пояснение. Если просто округлять, то 2.5 -> 2, 4.5 -> 4 etc, Особенность питона...
+# При увеличении на 0.1 гарантировано при чётном количестве - округление в меньшие сторону
+# при нечётном - в бОльшую
+print(f'Полученный список:\n{mul}')
